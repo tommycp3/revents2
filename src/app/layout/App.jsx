73632e4f24a216ab696695e78462dcc40dbffbd5
Thomas Miller <thomasmiller@Thomass-MacBook.local>
2018-08-12 02:10:49 +1000

    style navbar

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,16 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import EventDashboard from "../../features/event/EventDashboard/EventDashboard";
-import NavBar from '../../features/nav/NavBar/NavBar'
+import NavBar from "../../features/nav/NavBar/NavBar";
+import { Container } from "semantic-ui-react";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-       <NavBar />
-        <EventDashboard />
+      <div>
+        <NavBar />
+        <Container className="main">
+          <EventDashboard />
+        </Container>
       </div>
-    )
+    );
   }
 }
 
